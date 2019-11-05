f065cae5132bfd6419c9160b7a6775bcb4db3130
novand23 <novand23@gmail.com>
2019-11-05 22:06:39 +0200

    CRUD

--- src/main/java/api/repository/impl/UserRepository.java
+++ src/main/java/api/repository/impl/UserRepository.java
@@ -4,9 +4,8 @@ import api.entity.User;
 import api.repository.IUserRepository;
 import api.util.DbUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository implements IUserRepository {
@@ -19,7 +18,9 @@ public class UserRepository implements IUserRepository {
     @Override
     public void add(User user) {
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("");
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO people (firstname, secondname) VALUES (?,?);");
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getSecondName());
             preparedStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -28,20 +29,47 @@ public class UserRepository implements IUserRepository {
 
     @Override
     public void delete(int id) {
-        /*connection.prepareStatement()*/
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM people WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public List<User> findAll() {
-        /*connection.createStatement();*/
+        List<User> users = new ArrayList<>();
 
-        return null;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM people");
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setFirstName(resultSet.getString("firstname"));
+                user.setSecondName(resultSet.getString("secondname"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     @Override
     public void update(User user) {
-        /*connection.prepareStatement();*/
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE people SET firstname=?, secondname=? WHERE id=?");
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getSecondName());
+            preparedStatement.setInt(3, user.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
